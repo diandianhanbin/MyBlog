a0bf91ef64d7e922b06b82be4fd19d62e748c109
SvenWeng <234827102@qq.com>
2016-01-10 10:06:26 +0800

    修改form表单之前的提交

--- grzx/views.py
+++ grzx/views.py
@@ -5,7 +5,7 @@ import time
 
 def index(request):
     userinfo = UserInfo.objects.first()
-    blog_body = BlogBody.objects.all()[:6]
+    blog_body = BlogBody.objects.all()[:6:-1]
     return render(request,
                   'index.html',
                   {'userinfo': userinfo, 'blog_body': blog_body})
@@ -25,24 +25,24 @@ def article(request, blog_body_id=''):
 
 
 def python(request):
-    # python_blog = BlogBody.objects.filter(blog_type='Python')
-    sql = 'select id, blog_title, blog_type, blog_timestamp, blog_body from grzx_blogbody WHERE blog_type = "Python"'
-    python_blog = BlogBody.objects.raw(sql)
+    python_blog = BlogBody.objects.filter(blog_type='Python')[::-1]
+    # sql = 'select id, blog_title, blog_type, blog_timestamp, blog_body from grzx_blogbody WHERE blog_type = "Python"'
+    # python_blog = BlogBody.objects.raw(sql)
     return render(request, 'python_list.html', {'python_blog': python_blog})
 
 
 def abouttest(request):
-    test_blog = BlogBody.objects.filter(blog_type='abouttest')
+    test_blog = BlogBody.objects.filter(blog_type='abouttest')[::-1]
     return render(request, 'test_list.html', {'test_blog': test_blog})
 
 
 def mytalk(request):
-    mytalk_blog = BlogBody.objects.filter(blog_type='mytalk')
+    mytalk_blog = BlogBody.objects.filter(blog_type='mytalk')[::-1]
     return render(request, 'mytalk_list.html', {'mytalk_blog': mytalk_blog})
 
 
 def diary(request):
-    diary_blog = BlogBody.objects.filter(blog_type='diary')
+    diary_blog = BlogBody.objects.filter(blog_type='diary')[::-1]
     return render(request, 'diary_list.html', {'diary_blog': diary_blog})
 
 
@@ -55,6 +55,6 @@ def sub_article(request):
         mytype = request.GET['article_type']
         title = request.GET['article_title']
         body = request.GET['article_editor']
-        updb = BlogBody(blog_title=title, blog_body=body, blog_type=mytype, blog_timestamp=time.strftime("%Y-%m-%d %X", time.localtime()))
+        updb = BlogBody(blog_title=title, blog_body=body, blog_type=mytype, blog_timestamp=time.strftime("%Y-%m-%d %X", time.localtime()), blog_author='点点寒彬')
         updb.save()
         return redirect('/grzx/'+mytype)
